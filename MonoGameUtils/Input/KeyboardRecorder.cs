eca63a2252b2a88b407e3f45e3efdf56df721f13
Daniel Neel <dneelyep@gmail.com>
2014-09-24 21:11:26 -0400

    Remove un-used using statements in the solution.

--- MonoGameUtils/Input/KeyboardRecorder.cs
+++ MonoGameUtils/Input/KeyboardRecorder.cs
@@ -3,8 +3,6 @@ using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MonoGameUtils.Input
 {

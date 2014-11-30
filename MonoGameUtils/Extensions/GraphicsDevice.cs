fd05f0653a10bad27adf7638591c7c7ca570a64f
Daniel Neel <dneelyep@gmail.com>
2014-11-29 22:32:03 -0500

    Add an extension method to make it easy to access the screen's center coordinates.

---
+++ MonoGameUtils/Extensions/GraphicsDevice.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoGameUtils.Extensions
+{
+    public static class GraphicsDeviceExtensions
+    {
+        /// <summary>
+        /// A <see cref="Vector2"/> representing the center of the
+        /// provided <see cref="GraphicsDevice"/>'s <see cref="Viewport"/>.
+        /// </summary>
+        public static Vector2 ScreenCenter(this GraphicsDevice graphics)
+        {
+            return new Vector2(graphics.Viewport.Bounds.X/2,
+                               graphics.Viewport.Bounds.Y/2);
+        }
+    }
+}

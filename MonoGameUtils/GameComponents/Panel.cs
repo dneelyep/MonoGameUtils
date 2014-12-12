8dfa193e5eae915a6eb5f0ef5750d921cc367dad
Daniel Neel <dneelyep@gmail.com>
2014-12-11 19:08:42 -0500

    Add a Panel class, to make drawing rectangles easy.

---
+++ MonoGameUtils/GameComponents/Panel.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace MonoGameUtils.GameComponents
+{
+    /// <summary>
+    /// A solid-color rectangle that can be drawn on-screen.
+    /// </summary>
+    internal class Panel : DrawableGameComponent
+    {
+        #region "Properties"
+        public Rectangle Bounds { get; set; }
+        #endregion
+
+        private readonly Color BACKGROUND_COLOR;
+
+        private Texture2D Texture { get; set; }
+
+        private SpriteBatch mSpriteBatch;
+
+        public Panel(Rectangle bounds, Color backgroundColor, SpriteBatch spriteBatch, Game game) : base(game)
+        {
+            Bounds = bounds;
+            BACKGROUND_COLOR = backgroundColor;
+            mSpriteBatch = spriteBatch;
+        }
+
+        public override void Initialize()
+        {
+            Texture = new Texture2D(Game.GraphicsDevice, 1, 1);
+            Texture.SetData<Color>(new Color[] { BACKGROUND_COLOR } );
+
+            base.Initialize();
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            mSpriteBatch.Draw(Texture, Bounds, BACKGROUND_COLOR);
+
+            base.Draw(gameTime);
+        }
+    }
+}

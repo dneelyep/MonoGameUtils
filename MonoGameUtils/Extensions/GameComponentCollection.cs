5cb0ba077e356f8f58606bae044f007103a8f151
Daniel Neel <dneelyep@gmail.com>
2014-12-13 23:36:18 -0500

    Add the ability to add a set of gamecomponents to a game collection, rather than having to add each component individually.

---
+++ MonoGameUtils/Extensions/GameComponentCollection.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using Microsoft.Xna.Framework;
+
+namespace MonoGameUtils.Extensions
+{
+    public static class GameComponentCollectionExtensions
+    {
+        public static void AddRange(this GameComponentCollection gameComponents, IEnumerable<GameComponent> collection)
+        {
+            Contract.Requires(gameComponents != null);
+            Contract.Requires(collection != null);
+
+            foreach (GameComponent component in collection)
+            {
+                gameComponents.Add(component);
+            }
+        }
+    }
+}

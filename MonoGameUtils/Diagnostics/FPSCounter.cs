ff99ced514189bbf394d19ecb86a58fd3495afa0
Daniel Neel <dneelyep@gmail.com>
2016-12-31 16:38:35 -0500

    Add the ability to track/view a game's current FPS.

---
+++ MonoGameUtils/Diagnostics/FPSCounter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using System.Linq;
+
+namespace MonoGameUtils.Diagnostics
+{
+    /// <summary>
+    /// Reports back on the game's frames per second.
+    /// </summary>
+    /// <remarks>
+    /// Adapted from this StackOverflow answer: http://stackoverflow.com/a/20679895/848199
+    /// </remarks>
+    public class FPSCounter
+    {
+        public long TotalFrames { get; private set; }
+        public float TotalSeconds { get; private set; }
+        public float CurrentFPS { get; private set; }
+
+        public const int MAXIMUM_SAMPLES = 100;
+
+        public void Update(float deltaTime)
+        {
+            CurrentFPS = 1.0f / deltaTime;
+
+            TotalFrames++;
+            TotalSeconds += deltaTime;
+        }
+    }
+}

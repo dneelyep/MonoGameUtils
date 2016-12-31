12732c1dcbf93970664530bf07eaf4d0ad946534
Daniel Neel <dneelyep@gmail.com>
2016-12-31 17:03:00 -0500

    Add the ability for the FPSCounter to keep track of the average FPS over a series of samples.

--- MonoGameUtils/Diagnostics/FPSCounter.cs
+++ MonoGameUtils/Diagnostics/FPSCounter.cs
@@ -15,12 +15,23 @@ namespace MonoGameUtils.Diagnostics
         public long TotalFrames { get; private set; }
         public float TotalSeconds { get; private set; }
         public float CurrentFPS { get; private set; }
+        public float AverageFPS { get; private set; }
 
         public const int MAXIMUM_SAMPLES = 100;
+        private Queue<float> FPSQueue { get; set; }
 
         public void Update(float deltaTime)
         {
             CurrentFPS = 1.0f / deltaTime;
+
+            if (FPSQueue.Count > MAXIMUM_SAMPLES)
+            {
+                FPSQueue.Dequeue();
+            }
+
+            FPSQueue.Enqueue(CurrentFPS);
+
+            AverageFPS = FPSQueue.Average();
 
             TotalFrames++;
             TotalSeconds += deltaTime;

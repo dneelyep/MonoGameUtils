6423bb3bf09d7ad3f881c0255d344bb0b03359e8
Daniel Neel <dneelyep@gmail.com>
2017-01-01 20:00:22 -0500

    Add an extension method to make it simpler to retrieve a Rectangle's location as a Vector2.

---
+++ MonoGameUtils/Extensions/Rectangle.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.Contracts;
+using Microsoft.Xna.Framework;
+
+namespace MonoGameUtils.Extensions
+{
+    public static class RectangleExtensions
+    {
+        /// <summary>
+        /// This <see cref="Rectangle"/>'s location, represented as a Vector2D rather than a Point.
+        /// </summary>
+        public static Vector2 LocationV2(this Rectangle rect)
+        {
+            Contract.Requires(rect != null);
+
+            return rect.Location.ToVector2();
+        }
+    }
+}

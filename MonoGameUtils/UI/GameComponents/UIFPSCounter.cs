8e4653586d1bd9f43c6d7bd91c56190d64804e14
Daniel Neel <dneelyep@gmail.com>
2017-09-01 17:25:08 -0400

    Add a UI for our FPSCounter component, to allow users to quickly display an FPS counter without having to write any code to draw it.

---
+++ MonoGameUtils/UI/GameComponents/UIFPSCounter.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGameUtils.Diagnostics;
+
+namespace PCGame.GameComponents
+{
+    public class UIFPSCounter : DrawableGameComponent
+    {
+        private FPSCounter _fpsCounter = new FPSCounter();
+        private SpriteBatch _spriteBatch;
+        private SpriteFont _font;
+        private Vector2 _topLeftPoint;
+        private Vector2 _avgFPSLocation;
+        private readonly Color _fontColor = Color.Black;
+
+        public UIFPSCounter(Game game,
+                            SpriteFont font,
+                            Vector2 topLeftPoint,
+                            SpriteBatch spriteBatch) : base(game)
+        {
+            _spriteBatch = spriteBatch;
+            _font = font;
+            _topLeftPoint = topLeftPoint;
+            _avgFPSLocation = Vector2.Add(_topLeftPoint, new Vector2(0, 20));
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            _fpsCounter.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            _spriteBatch.DrawString(_font, "Current FPS: " + _fpsCounter.CurrentFPS, _topLeftPoint, _fontColor);
+            _spriteBatch.DrawString(_font, "Avg FPS: " + _fpsCounter.AverageFPS, _avgFPSLocation, _fontColor);
+
+            base.Draw(gameTime);
+        }
+    }
+}

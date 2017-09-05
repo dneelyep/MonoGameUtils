70ae2a630931c74a4bdc64ee9f9df4798fb68d5b
Daniel Neel <dneelyep@gmail.com>
2017-09-04 22:13:12 -0400

    Add an extension method to make it simple to draw Models.

---
+++ MonoGameUtils/Extensions/Model.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoGameUtils.Extensions
+{
+    public static class ModelExtensions
+    {
+        public static void Draw(this Model model, Matrix world, Matrix view, Matrix projection)
+        {
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.World = world;
+                    effect.View = view;
+                    effect.Projection = projection;
+                }
+
+                mesh.Draw();
+            }
+        }
+    }
+}
